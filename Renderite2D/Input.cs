9de48222e1e847a60e29434e67e9ddfa9ae5b47f
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-04 19:33:33 +0300

    Added Input class

---
+++ Renderite2D/Input.cs
@@ -0,0 +1,42 @@
+﻿using Renderite2D_Project.EngineCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderite2D_Project.EngineCode
+{
+    public static class Input
+    {
+        /// <summary>
+        /// Returns a bool value that will indicate whether a key is being held down.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsKeyDown(OpenTK.Windowing.GraphicsLibraryFramework.Keys key)
+        {
+            return Program.GameWindow.IsKeyDown(key);
+        }
+
+        /// <summary>
+        /// Returns a bool value that will indicate whether a key is currently not being held down.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsKeyUp(OpenTK.Windowing.GraphicsLibraryFramework.Keys key)
+        {
+            return Program.GameWindow.IsKeyReleased(key);
+        }
+
+        /// <summary>
+        /// Will return true when the specified key is currently pressed, but was not in the previous frame.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsKeyPressed(OpenTK.Windowing.GraphicsLibraryFramework.Keys key)
+        {
+            return Program.GameWindow.IsKeyPressed(key);
+        }
+    }
+}
