02a6118765f9d50534a2dead6e469f764a841cf8
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-04 19:34:01 +0300

    Added SampleLevel

---
+++ SampleLevel.cs
@@ -0,0 +1,22 @@
+﻿using Renderite2D_Project.EngineCode;
+
+namespace Renderite2D_Project
+{
+    public class SampleLevel : Level
+    {
+        protected override void Begin()
+        {
+
+        }
+
+        protected override void Update()
+        {
+
+        }
+
+        protected override void End()
+        {
+
+        }
+    }
+}
