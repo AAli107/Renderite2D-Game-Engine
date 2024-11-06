e80069132283ecb2903ce69e6f39855e58d5c208
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-06 14:49:16 +0300

    Added Component

---
+++ Renderite2D/Component.cs
@@ -0,0 +1,11 @@
+﻿namespace Renderite2D_Project.Renderite2D
+{
+    public abstract class Component
+    {
+        public bool IsEnabled { get { return isEnabled; } set { isEnabled = value; } }
+
+        bool isEnabled = true;
+
+        public abstract void Update();
+    }
+}
