14afb3fd8740760214019a76a4e6d54c9cd6557e
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-06 14:49:03 +0300

    Added Transform2D

---
+++ Renderite2D/Transform2D.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK.Mathematics;
+
+namespace Renderite2D_Project.Renderite2D
+{
+    public struct Transform2D
+    {
+        public Vector2d position = Vector2d.Zero;
+        public float rotation = 0.0f;
+        public Vector2d scale = Vector2d.One;
+
+        public Transform2D() { }
+    }
+}
