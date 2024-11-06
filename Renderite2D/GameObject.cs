334614ffd2ff0511a8eea4af6a66a3299e26bb48
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-06 14:49:23 +0300

    Added GameObject

---
+++ Renderite2D/GameObject.cs
@@ -0,0 +1,47 @@
+﻿namespace Renderite2D_Project.Renderite2D
+{
+    public class GameObject
+    {
+        public bool IsEnabled { get { return isEnabled; } set { isEnabled = value; } }
+
+        public Transform2D transform = new();
+        
+        bool isEnabled = true;
+        readonly List<Component> components = new();
+
+        public void Update()
+        {
+            if (!IsEnabled) return;
+
+            for (int i = 0; i < components.Count; i++)
+                if (components[i] != null && components[i].IsEnabled)
+                    components[i]?.Update();
+        }
+
+        public Component[] GetAllComponents()
+        {
+            return components.ToArray();
+        }
+
+        public Component GetComponent<T>() where T : Component
+        {
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (components[i] is T)
+                    return components[i];
+            }
+            return null;
+        }
+
+        public Component[] GetComponents<T>() where T : Component
+        {
+            List<Component> foundComponents = new();
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (components[i] is T)
+                    foundComponents.Add(components[i]);
+            }
+            return foundComponents.ToArray();
+        }
+    }
+}
