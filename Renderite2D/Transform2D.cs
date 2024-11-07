b7553d79e83a01bbcdede46883d4baf9364b2a8e
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-07 14:22:52 +0300

    Removed unused variable

--- Renderite2D/Transform2D.cs
+++ Renderite2D/Transform2D.cs
@@ -5,7 +5,6 @@ namespace Renderite2D_Project.Renderite2D
     public struct Transform2D
     {
         public Vector2d position = Vector2d.Zero;
-        public float rotation = 0.0f;
         public Vector2d scale = Vector2d.One;
 
         public Transform2D() { }
