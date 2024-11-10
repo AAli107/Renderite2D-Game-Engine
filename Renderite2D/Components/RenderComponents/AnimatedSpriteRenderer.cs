819a720dc19170990712b382a2b2ef54db838bae
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-10 16:11:04 +0300

    Added AnimatedSpriteRenderer component.

---
+++ Renderite2D/Components/RenderComponents/AnimatedSpriteRenderer.cs
@@ -0,0 +1,42 @@
+﻿namespace Renderite2D_Project.Renderite2D.Components.RenderComponents
+{
+    public class AnimatedSpriteRenderer : SpritesheetRenderer
+    {
+        public double TimePerFrame {  
+            get
+            {
+                return timePerFrame;
+            }
+            set
+            {
+                timePerFrame = value < 0 ? 0 : value;
+            }
+        }
+        public int EndFrameIndex { get { return endFrameIndex; } set { endFrameIndex = value < 0 ? 0 : value; } }
+        public bool IsPlaying { get { return isPlaying; } set { isPlaying = value; } }
+        public bool PlayReverse { get; set; }
+
+        private double timePerFrame = Game.Time.FixedDeltaTime * 10;
+        private double animTime = 0;
+        private int endFrameIndex = 0;
+        private bool isPlaying = true;
+
+        public AnimatedSpriteRenderer(GameObject parent) : base(parent) { }
+
+        public override void FixedUpdate()
+        {
+            if (IsPlaying)
+            {
+                if (animTime >= timePerFrame)
+                {
+                    animTime = 0;
+                    index = PlayReverse ? 
+                        (index <= 0 ? endFrameIndex : index - 1) : 
+                        (index >= endFrameIndex ? 0 : index + 1);
+                }
+                animTime += Game.Time.FixedDeltaTime;
+            }
+            else animTime = 0;
+        }
+    }
+}
--- SampleLevel.cs
+++ SampleLevel.cs
@@ -1,5 +1,4 @@
-﻿using NAudio.Wave;
-using OpenTK.Mathematics;
+﻿using OpenTK.Mathematics;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using Renderite2D_Project.Renderite2D;
 using Renderite2D_Project.Renderite2D.Components;
@@ -19,8 +18,7 @@ namespace Renderite2D_Project
         PhysicsComponent pc;
         ColliderComponent cc;
         AudioComponent ac;
-        SpritesheetRenderer ssr;
-        int animTime = 0;
+        AnimatedSpriteRenderer asr;
 
         public override void Begin()
         {
@@ -30,10 +28,11 @@ namespace Renderite2D_Project
             pc = gameObjectTest.AddComponent<PhysicsComponent>();
             cc = gameObjectTest.AddComponent<ColliderComponent>();
             ac = gameObjectTest.AddComponent<AudioComponent>();
-            ssr = gameObjectTest.AddComponent<SpritesheetRenderer>();
-            ssr.texture = spriteSheetTex;
-            ssr.divisions = 3;
-            ssr.layer = 1;
+            asr = gameObjectTest.AddComponent<AnimatedSpriteRenderer>();
+            asr.texture = spriteSheetTex;
+            asr.divisions = 3;
+            asr.layer = 1;
+            asr.EndFrameIndex = 5;
             ac.FilePath = "Assets/Game Assets/pick.wav";
             gameObjectTest2 = new(new Vector2d(500, 600));
             var cc2 = gameObjectTest2.AddComponent<ColliderComponent>();
@@ -78,13 +77,6 @@ namespace Renderite2D_Project
             if (Input.IsKeyDown(Keys.A)) { pc.AddVelocity(-Vector2d.UnitX); }
             if (Input.IsKeyDown(Keys.S)) { pc.AddVelocity(Vector2d.UnitY); }
             if (Input.IsKeyDown(Keys.D)) { pc.AddVelocity(Vector2d.UnitX); }
-
-            if (animTime >= 10)
-            {
-                animTime = 0;
-                ssr.index = ssr.index >= 5 ? 0 : ssr.index + 1;
-            }
-            else animTime++;
         }
 
         public override void Draw(Game.Shapes gfx)
@@ -95,17 +87,14 @@ namespace Renderite2D_Project
             Game.DrawShape(Game.DrawType.Text, new object[] { new Vector2d(200, 200), "Layered Drawing", Color4.Cyan, 4f, false}, 0);
 
             gfx.DrawQuad(new Vector2d(300, 300), new Vector2d(500, 300), new Vector2d(300, 500), new Vector2d(500, 500), Color4.White, spriteSheetTex);
-            gfx.DrawQuad(new Vector2d(100, 100), new Vector2d(200, 100), new Vector2d(100, 200), new Vector2d(200, 200), Color4.Yellow);
+            gfx.DrawQuad(new Vector2d(100, 100), new Vector2d(200, 100), new Vector2d(100, 200), new Vector2d(200, 200), Color4.Yellow, nTex);
             gfx.DrawLine(v, new Vector2d(150, 150), Color4.Violet, (float)Math.Sin(Game.Time.TimeSinceLevelStart) * 25f);
-            gfx.DrawTriangle(new Vector2d(800, 200), new Vector2d(700, 200), new Vector2d(600, 300), Color4.Magenta);
-            gfx.DrawPixel(new Vector2i(320, 240), Color4.Red);
-            gfx.DrawPoint(new Vector2i(350, 240), Color4.Lime, (float)Math.Sin(Game.Time.TimeSinceLevelStart) * 10f);
             gfx.DrawText(Vector2d.Zero, Math.Round(Game.Time.FPS) + " FPS", Color4.Red, 1);
             gfx.DrawText(Vector2d.UnitY * 28, "Time Since Level start = " + Game.Time.TimeSinceLevelStart, Color4.Red, 1);
             gfx.DrawText(Vector2d.UnitY * 56, "TimeScale = " + Game.Time.TimeScale, Color4.Red, 1);
             gfx.DrawText(Vector2d.UnitY * 84, "MouseScreenPos = " + Input.MouseScreenPos, Color4.Red, 1);
             gfx.DrawText(Vector2d.UnitY * 112, "MouseWorldPos = " + Input.MouseWorldPos, Color4.Red, 1);
-            gfx.DrawText(Vector2d.UnitY * 140, "animationIndex = " + ssr.index, Color4.Red, 1);
+            gfx.DrawText(Vector2d.UnitY * 140, "animationIndex = " + asr.index, Color4.Red, 1);
         }
     }
 }
