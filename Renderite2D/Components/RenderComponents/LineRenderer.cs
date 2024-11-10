aeb4ef560a9dcf6829761fd122017e076ad922c7
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-10 15:15:57 +0300

    Added LineRenderer component.

---
+++ Renderite2D/Components/RenderComponents/LineRenderer.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Mathematics;
+
+namespace Renderite2D_Project.Renderite2D.Components.RenderComponents
+{
+    public class LineRenderer : RenderComponent
+    {
+        public Vector2d pointA = new(-50, -50);
+        public Vector2d pointB = new(50, 50);
+        public Color4 color = Color4.White;
+        public float width = 1f;
+        public bool isStatic = false;
+
+        public LineRenderer(GameObject parent) : base(parent) 
+        {
+            drawType = Game.DrawType.Line;
+        }
+
+        public override void Update()
+        {
+            parameters = new object[5] 
+            {
+                pointA + Parent.transform.position, 
+                pointB + Parent.transform.position,
+                color, 
+                width, 
+                isStatic 
+            };
+            base.Update();
+        }
+    }
+}
