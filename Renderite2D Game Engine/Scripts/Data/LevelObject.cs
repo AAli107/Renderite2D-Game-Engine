d3350afe759c8fbe11fe4adbe5a12c22bd4b8883
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-15 17:38:30 +0300

    Added LevelObject struct

---
+++ Renderite2D Game Engine/Scripts/Data/LevelObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Renderite2D_Game_Engine.Scripts.Data
+{
+    [Serializable]
+    public struct LevelObject
+    {
+        public double x;
+        public double y;
+        public List<LevelComponent> components;
+        public bool isEnabled;
+
+        public LevelObject(double x, double y, List<LevelComponent> components, bool isEnabled = true)
+        {
+            this.x = x;
+            this.y = y;
+            this.components = components;
+            this.isEnabled = isEnabled;
+        }
+    }
+}
