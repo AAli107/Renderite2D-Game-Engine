e9b57cc6d75dc5caba1671be7e1d24e89fe8d0c6
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-15 17:38:21 +0300

    Added LevelComponent struct

---
+++ Renderite2D Game Engine/Scripts/Data/LevelComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderite2D_Game_Engine.Scripts.Data
+{
+    [Serializable]
+    public struct LevelComponent
+    {
+        public string componentType;
+        public Dictionary<string, object> values;
+        public bool isEnabled;
+
+        public LevelComponent(string componentType, Dictionary<string, object> values, bool isEnabled = true)
+        {
+            this.componentType = componentType;
+            this.values = values;
+            this.isEnabled = isEnabled;
+        }
+    }
+}
