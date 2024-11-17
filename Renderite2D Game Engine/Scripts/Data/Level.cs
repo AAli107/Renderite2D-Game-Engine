7eafe2f01dd03100b056f55b0a7d85fa22f5b9cd
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-17 13:17:38 +0300

    Added Level struct

---
+++ Renderite2D Game Engine/Scripts/Data/Level.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Renderite2D_Game_Engine.Scripts.Data
+{
+    [Serializable]
+    public struct Level
+    {
+        public Color backgroundColor;
+        public List<LevelObject> gameObjects;
+
+        public Level()
+        {
+            backgroundColor = Color.Black;
+            gameObjects = new();
+        }
+
+        public Level(Color backgroundColor, List<LevelObject> gameObjects)
+        {
+            this.backgroundColor = backgroundColor;
+            this.gameObjects = gameObjects;
+        }
+    }
+}
