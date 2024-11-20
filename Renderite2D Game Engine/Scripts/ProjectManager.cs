6f0f6b6fafedb0dad15e024543f8d9af54ea9fa5
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-20 13:10:59 +0300

    Added ProjectManager class.

---
+++ Renderite2D Game Engine/Scripts/ProjectManager.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Renderite2D_Game_Engine.Scripts.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renderite2D_Game_Engine.Scripts
+{
+    public static class ProjectManager
+    {
+        public static bool IsProjectOpen { get; private set; }
+        public static string ProjectPath { get; private set; }
+        public static string CurrentLevelPath { get; private set; }
+        public static Project ProjectData { get; private set; }
+        public static Level CurrentLevelData { get; private set; }
+
+        public static bool LoadLevel(string levelPath)
+        {
+            if (!IsProjectOpen) return false;
+
+            try {
+                var settings = new JsonSerializerSettings()
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error,
+                };
+                CurrentLevelData = JsonConvert.DeserializeObject<Level>(File.ReadAllText(levelPath), settings);
+                CurrentLevelPath = levelPath;
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Invalid or Corrupt Level File...\n" + ex.Message,
+                    "Level Loading Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public static bool LoadProject(string projectPath)
+        {
+            try
+            {
+                var settings = new JsonSerializerSettings()
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error,
+                };
+                ProjectData = JsonConvert.DeserializeObject<Project>(File.ReadAllText(projectPath), settings);
+                ProjectPath = projectPath;
+                IsProjectOpen = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid or Corrupt Level File...\n" + ex.Message,
+                    "Level Loading Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public static void CloseProject()
+        {
+            if (IsProjectOpen)
+            {
+                IsProjectOpen = false;
+                ProjectPath = null;
+                CurrentLevelPath = null;
+                ProjectData = default;
+                CurrentLevelData = default;
+            }
+        }
+
+    }
+}
