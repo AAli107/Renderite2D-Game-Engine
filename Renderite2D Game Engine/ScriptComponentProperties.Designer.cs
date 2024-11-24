5033b358dcb0ddec380ae4ad4d0da8e7f0c7d732
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-24 17:23:21 +0300

    Added ScriptComponentProperties.

---
+++ Renderite2D Game Engine/ScriptComponentProperties.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Renderite2D_Game_Engine
+{
+    partial class ScriptComponentProperties
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Size = new System.Drawing.Size(234, 126);
+            // 
+            // componentName_label
+            // 
+            this.componentName_label.Text = "Custom Script Component";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(3, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Class Name :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 27);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Corbel", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(3, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 43);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Important Information :\r\n- Class must extend from ScriptComponent.\r\n- Be sure to " +
+    "check your spelling.\r\n";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // ScriptComponentProperties
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.Name = "ScriptComponentProperties";
+            this.Size = new System.Drawing.Size(238, 185);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Renderite2D Game Engine/ScriptComponentProperties.cs
@@ -0,0 +1,59 @@
+﻿using Renderite2D_Game_Engine.Scripts.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Renderite2D_Game_Engine
+{
+    public partial class ScriptComponentProperties : Renderite2D_Game_Engine.Component_Properties
+    {
+        [Obsolete("Designer only", true)]
+        public ScriptComponentProperties()
+        {
+            InitializeComponent();
+        }
+
+        public ScriptComponentProperties(LevelEditor levelEditor, string componentId) : base(levelEditor, componentId)
+        {
+            InitializeComponent();
+        }
+
+        protected override void UpdateComponent_(LevelComponent component)
+        {
+            textBox1.Text = (string)component.values["ScriptClass"];
+        }
+
+        public override int GetHeight()
+        {
+            return 185;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace(" ", "").Trim();
+
+            if (textBox1.Text == "ScriptComponent") 
+                textBox1.Text = "";
+
+            bool characterCheck = true;
+            if (textBox1.Text.Length > 1)
+            {
+                for (int i = 1; i < textBox1.Text.Length; i++)
+                {
+                    if (!char.IsLetterOrDigit(textBox1.Text[i]))
+                    {
+                        characterCheck = false;
+                        break;
+                    }
+                }
+            }
+
+            if ((char.IsLetter(textBox1.Text[0]) || textBox1.Text[0] == '_' || textBox1.Text[0] == '@') && characterCheck) 
+                SetComponentValue("ScriptClass", textBox1.Text);
+        }
+    }
+}
