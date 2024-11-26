bc4aeff18ccdb777a1514b0c0f3e56150fcd2dde
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-26 17:18:13 +0300

    Added CreateDialog

---
+++ Renderite2D Game Engine/CreateDialog.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Renderite2D_Game_Engine
+{
+    partial class CreateDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateDialog));
+            this.title_label = new System.Windows.Forms.Label();
+            this.nameInput_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.create_btn = new System.Windows.Forms.Button();
+            this.cancel_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title_label
+            // 
+            this.title_label.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.title_label.Font = new System.Drawing.Font("Corbel", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_label.Location = new System.Drawing.Point(12, 9);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(280, 52);
+            this.title_label.TabIndex = 0;
+            this.title_label.Text = "Create";
+            this.title_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameInput_textbox
+            // 
+            this.nameInput_textbox.Location = new System.Drawing.Point(36, 94);
+            this.nameInput_textbox.Name = "nameInput_textbox";
+            this.nameInput_textbox.Size = new System.Drawing.Size(236, 27);
+            this.nameInput_textbox.TabIndex = 1;
+            this.nameInput_textbox.Text = "file_name";
+            this.nameInput_textbox.TextChanged += new System.EventHandler(this.nameInput_textbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name :";
+            // 
+            // create_btn
+            // 
+            this.create_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.create_btn.Location = new System.Drawing.Point(36, 150);
+            this.create_btn.Name = "create_btn";
+            this.create_btn.Size = new System.Drawing.Size(102, 39);
+            this.create_btn.TabIndex = 3;
+            this.create_btn.Text = "Create";
+            this.create_btn.UseVisualStyleBackColor = false;
+            this.create_btn.Click += new System.EventHandler(this.create_btn_Click);
+            // 
+            // cancel_btn
+            // 
+            this.cancel_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.cancel_btn.Location = new System.Drawing.Point(170, 150);
+            this.cancel_btn.Name = "cancel_btn";
+            this.cancel_btn.Size = new System.Drawing.Size(102, 39);
+            this.cancel_btn.TabIndex = 4;
+            this.cancel_btn.Text = "Cancel";
+            this.cancel_btn.UseVisualStyleBackColor = false;
+            this.cancel_btn.Click += new System.EventHandler(this.cancel_btn_Click);
+            // 
+            // CreateDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(304, 201);
+            this.Controls.Add(this.cancel_btn);
+            this.Controls.Add(this.create_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameInput_textbox);
+            this.Controls.Add(this.title_label);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CreateDialog";
+            this.Text = "Renderite2D Game Engine - Create";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CreateDialog_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title_label;
+        private System.Windows.Forms.TextBox nameInput_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button create_btn;
+        private System.Windows.Forms.Button cancel_btn;
+    }
+}
---
+++ Renderite2D Game Engine/CreateDialog.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.CSharp;
+using Newtonsoft.Json;
+using Renderite2D_Game_Engine.Scripts;
+using Renderite2D_Game_Engine.Scripts.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renderite2D_Game_Engine
+{
+    public partial class CreateDialog : Form
+    {
+        public string fileName = "";
+
+        readonly AssetType assetType;
+        readonly string[] fileEntries;
+        readonly string currentDirectory;
+
+        public CreateDialog(AssetType assetType, string currentDirectory)
+        {
+            InitializeComponent();
+            this.assetType = assetType;
+            this.currentDirectory = currentDirectory;
+
+            title_label.Text = assetType switch
+            {
+                AssetType.Folder => "Create Folder",
+                AssetType.Level => "Create Level",
+                AssetType.EmptyClass => "Create Empty Class",
+                AssetType.CustomScript => "Create Script",
+                AssetType.GameObjectScript => "Create Game Object",
+                _ => "Create",
+            };
+
+
+            if (assetType == AssetType.Folder)
+            {
+                string[] dirs = Directory.GetDirectories(currentDirectory);
+                fileEntries = new string[dirs.Length];
+
+                for (int i = 0; i < dirs.Length; i++)
+                    fileEntries[i] = Path.GetDirectoryName(dirs[i]);
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(currentDirectory);
+                fileEntries = new string[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                    fileEntries[i] = Path.GetFileNameWithoutExtension(files[i]);
+            }
+
+            string gameObjectBaseName = assetType switch
+            {
+                AssetType.Folder => "Folder",
+                AssetType.Level => "Level",
+                AssetType.EmptyClass => "Class",
+                AssetType.CustomScript => "Script",
+                AssetType.GameObjectScript => "Game Object Script",
+                AssetType.TopDownCharacter => "Topdown Character",
+                AssetType.SideScrollerCharacter => "Side Scroller Character",
+                _ => "",
+            };
+
+            int index = 0;
+            string inputFileName;
+
+            inputFileName = gameObjectBaseName;
+            while (ProjectManager.CurrentLevelData.gameObjects.ContainsKey(inputFileName))
+            {
+                index++;
+                inputFileName = gameObjectBaseName + "_" + index;
+            }
+            nameInput_textbox.Text = inputFileName;
+        }
+
+        private bool DoesInputNameExist()
+        {
+            return DoesFileExist(fileName);
+        }
+
+        private bool DoesFileExist(string fileName)
+        {
+
+            return fileEntries.Contains(fileName.Trim());
+        }
+
+        private bool ContainsInvalidCharacters()
+        {
+            string str = fileName;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((assetType != AssetType.Folder && Path.GetInvalidFileNameChars().Contains(str[i])) ||
+                    (assetType == AssetType.Folder && Path.GetInvalidPathChars().Contains(str[i])))
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool IsValidIdentifier(string str)
+        {
+            return new CSharpCodeProvider().IsValidIdentifier(str);
+        }
+
+        private bool IsNameEmpty()
+        {
+            return fileName == "";
+        }
+
+        private bool IsValidName()
+        {
+            return !IsNameEmpty() && !DoesInputNameExist() && !ContainsInvalidCharacters();
+        }
+
+        private void nameInput_textbox_TextChanged(object sender, EventArgs e)
+        {
+            fileName = nameInput_textbox.Text.Trim();
+        }
+
+        private void cancel_btn_Click(object sender, EventArgs e)
+        {
+            if (DialogResult != DialogResult.OK)
+                DialogResult = DialogResult.Cancel;
+        }
+
+        private void CreateDialog_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (DialogResult != DialogResult.OK)
+                DialogResult = DialogResult.Cancel;
+        }
+
+        private void create_btn_Click(object sender, EventArgs e)
+        {
+            if (IsValidName())
+            {
+                try
+                {
+                    if (assetType == AssetType.Folder)
+                    {
+                        Directory.CreateDirectory(currentDirectory + '\\' + fileName);
+                    }
+                    else
+                    {
+                        string fileformat = assetType == AssetType.Level ? ".rdlvl" : ".cs";
+                        string fileContents = assetType switch
+                        {
+                            AssetType.Level => JsonConvert.SerializeObject(new Level(), Formatting.Indented),
+                            AssetType.EmptyClass => InsertClassNameToScript(GetEmptyClassTemplate(), fileName),
+                            AssetType.CustomScript => InsertClassNameToScript(GetCustomScriptTemplate(), fileName),
+                            AssetType.GameObjectScript => InsertClassNameToScript(GetGameObjectTemplate(), fileName),
+                            AssetType.SideScrollerCharacter => InsertClassNameToScript(GetSideScrollerCharacterTemplate(), fileName),
+                            AssetType.TopDownCharacter => InsertClassNameToScript(GetTopDownCharacterTemplate(), fileName),
+                            _ => "",
+                        };
+                        File.WriteAllText(currentDirectory + '\\' + fileName + fileformat, fileContents);
+                    }
+                    DialogResult = DialogResult.OK;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "[Message] " + ex.Message + "\n\n" +
+                        "[Source] " + ex.Source + "\n\n" +
+                        "[Stack Trace]\n" + ex.StackTrace,
+                    "Exception Caught!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    DialogResult = DialogResult.Cancel;
+                }
+            }
+            else
+            {
+                bool isFolder = assetType == AssetType.Folder;
+                string message = "Could not Create" + (isFolder ? "folder" : "file") + "...\n\nFail Reason(s):\n";
+
+                if (IsNameEmpty())
+                    message += "- Name is Empty.\n";
+                if (DoesInputNameExist())
+                    message += "- " + (isFolder ? "Folder" : "File") + " with same name already exists.\n";
+                if (ContainsInvalidCharacters())
+                    message += "- " + (isFolder ? "Folder" : "File") + " contains invalid characters.\n";
+
+                MessageBox.Show(message, "Failed To Create!");
+            }
+        }
+
+        public static string GetEmptyClassTemplate()
+        {
+            return "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\nusing Renderite2D_Project.Renderite2D;\r\nusing Renderite2D_Project.Renderite2D.Components;\r\nusing Renderite2D_Project.Renderite2D.Components.RenderComponents;\r\nusing Renderite2D_Project.Renderite2D.Game_Features;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;\r\nusing Renderite2D_Project.Renderite2D.Graphics;\r\n\r\nnamespace Renderite2D_Project\r\n{\r\n    public class __class_name__\r\n    {\r\n\r\n    }\r\n}\r\n";
+        }
+
+        public static string GetCustomScriptTemplate()
+        {
+            return "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\nusing Renderite2D_Project.Renderite2D;\r\nusing Renderite2D_Project.Renderite2D.Components;\r\nusing Renderite2D_Project.Renderite2D.Components.RenderComponents;\r\nusing Renderite2D_Project.Renderite2D.Game_Features;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;\r\nusing Renderite2D_Project.Renderite2D.Graphics;\r\n\r\nnamespace Renderite2D_Project\r\n{\r\n    public class __class_name__ : ScriptComponent\r\n    {\r\n        public __class_name__(GameObject parent) : base(parent) { }\r\n        \r\n        public override void Start()\r\n        {\r\n            // Is called when script is first loaded (constructor)\r\n        }\r\n\r\n        public override void Update()\r\n        {\r\n            // Is called once per frame\r\n        }\r\n\r\n        public override void FixedUpdate()\r\n        {\r\n            // Is called fixed amount of times per second\r\n        }\r\n\r\n        public override void End()\r\n        {\r\n            // Is called when script is unloaded (destructor)\r\n        }\r\n    }\r\n}\r\n";
+        }
+
+        public static string GetGameObjectTemplate()
+        {
+            return "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\nusing Renderite2D_Project.Renderite2D;\r\nusing Renderite2D_Project.Renderite2D.Components;\r\nusing Renderite2D_Project.Renderite2D.Components.RenderComponents;\r\nusing Renderite2D_Project.Renderite2D.Game_Features;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;\r\nusing Renderite2D_Project.Renderite2D.Graphics;\r\n\r\nnamespace Renderite2D_Project\r\n{\r\n    public class __class_name__ : GameObject\r\n    {\r\n\r\n    }\r\n}\r\n";
+        }
+
+        public static string GetTopDownCharacterTemplate()
+        {
+            return "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\nusing Renderite2D_Project.Renderite2D;\r\nusing Renderite2D_Project.Renderite2D.Components;\r\nusing Renderite2D_Project.Renderite2D.Components.RenderComponents;\r\nusing Renderite2D_Project.Renderite2D.Game_Features;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;\r\nusing Renderite2D_Project.Renderite2D.Graphics;\r\n\r\nnamespace Renderite2D_Project\r\n{\r\n    public class __class_name__ : TopDownCharacter\r\n    {\r\n\r\n    }\r\n}\r\n";
+        }
+
+        public static string GetSideScrollerCharacterTemplate()
+        {
+            return "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\nusing Renderite2D_Project.Renderite2D;\r\nusing Renderite2D_Project.Renderite2D.Components;\r\nusing Renderite2D_Project.Renderite2D.Components.RenderComponents;\r\nusing Renderite2D_Project.Renderite2D.Game_Features;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;\r\nusing Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;\r\nusing Renderite2D_Project.Renderite2D.Graphics;\r\n\r\nnamespace Renderite2D_Project\r\n{\r\n    public class __class_name__ : SideScrollerCharacter\r\n    {\r\n\r\n    }\r\n}\r\n";
+        }
+
+        public static string InsertClassNameToScript(string script, string __class_name__)
+        {
+            return script.Replace("__class_name__", SanitizeClassName(__class_name__));
+        }
+
+        public static string SanitizeClassName(string name)
+        {
+            if (IsValidIdentifier(name))
+                return name;
+
+            name = name.Trim().Replace(" ", "_");
+
+            if (name.Length > 0 && !char.IsLetter(name[0]) && name[0] != '_' && name[0] != '@')
+                name = '_' + (name.Length > 1 ? name.Substring(1) : "");
+
+            string name2 = "";
+            char[] disallowedChars = """ \|!#$%&/()=?»«@£§€{}.-:;"'<>,`~*[] """.Trim(' ').ToCharArray();
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (i != 0 && disallowedChars.Contains(name[i]))
+                    name2 += '_';
+                else name2 += name[i];
+            }
+            name = name2;
+
+            if (!IsValidIdentifier(name))
+                name = "@" + name;
+
+            return name;
+        }
+    }
+
+    public enum AssetType
+    {
+        None,
+        Folder,
+        Level,
+        EmptyClass,
+        CustomScript,
+        GameObjectScript,
+        TopDownCharacter,
+        SideScrollerCharacter,
+    }
+}
