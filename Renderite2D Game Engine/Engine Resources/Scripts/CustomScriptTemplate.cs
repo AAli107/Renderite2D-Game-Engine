6f7e3d097f0399b91b14862d2b55317ff9be32b2
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-27 15:47:26 +0300

    Added Template scripts as files.

---
+++ Renderite2D Game Engine/Engine Resources/Scripts/CustomScriptTemplate.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renderite2D_Project.Renderite2D;
+using Renderite2D_Project.Renderite2D.Components;
+using Renderite2D_Project.Renderite2D.Components.RenderComponents;
+using Renderite2D_Project.Renderite2D.Game_Features;
+using Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;
+using Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;
+using Renderite2D_Project.Renderite2D.Graphics;
+
+namespace Renderite2D_Project
+{
+    public class __class_name__ : ScriptComponent
+    {
+        public __class_name__(GameObject parent) : base(parent) { }
+        
+        public override void Start()
+        {
+            // Is called when script is first loaded (constructor)
+        }
+        
+        public override void Update()
+        {
+            // Is called once per frame
+        }
+        
+        public override void FixedUpdate()
+        {
+            // Is called fixed amount of times per second
+            
+        }
+        
+        public override void End()
+        {
+            // Is called when script is unloaded (destructor)
+        }
+    }
+}
---
+++ Renderite2D Game Engine/Engine Resources/Scripts/TopDownCharacterTemplate.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renderite2D_Project.Renderite2D;
+using Renderite2D_Project.Renderite2D.Components;
+using Renderite2D_Project.Renderite2D.Components.RenderComponents;
+using Renderite2D_Project.Renderite2D.Game_Features;
+using Renderite2D_Project.Renderite2D.Game_Features.Game_Objects;
+using Renderite2D_Project.Renderite2D.Game_Features.Game_Objects.Characters;
+using Renderite2D_Project.Renderite2D.Graphics;
+
+namespace Renderite2D_Project
+{
+    public class __class_name__ : TopDownCharacter
+    {
+
+    }
+}
