3539777645034d42dec9499d05be67d2ed429a93
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-27 11:22:08 +0300

    Updated SampleLevel with background texture and color.

--- Renderite2D_Project/SampleLevel.cs
+++ Renderite2D_Project/SampleLevel.cs
@@ -12,29 +12,37 @@ namespace Renderite2D_Project
 {
     public class SampleLevel : Level
     {
+        readonly Texture __background_texture__ = 
+            new("__bg_texture_path_name__");
+        readonly Color __background_texture_color__ = 
+            Color.White; // __background_texture_color__
+
         public override void Begin()
         {
-
+            // __begin_code__
         }
 
         public override void Update()
         {
-
+            // __update_code__
         }
 
         public override void FixedUpdate()
         {
-
+            // __fixed_update_code__
         }
 
         public override void Draw(Game.Shapes gfx)
         {
-
+            if (__background_texture__ != null && !__background_texture__.IsMissingTexture)
+                gfx.DrawRectangle(new(0,0),new(1920,1080),
+                    __background_texture_color__, __background_texture__, true);
+            // __draw_code__
         }
 
         public override void End()
         {
-
+            // __end_code__
         }
     }
 }
