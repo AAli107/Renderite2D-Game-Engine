d47cdfad469b42537d501760d2db517e07fb0eef
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-27 16:41:53 +0300

    Added ToLevelScript() to Level struct (incomplete)

--- Renderite2D Game Engine/Scripts/Data/Level.cs
+++ Renderite2D Game Engine/Scripts/Data/Level.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 
 namespace Renderite2D_Game_Engine.Scripts.Data
@@ -66,6 +67,27 @@ namespace Renderite2D_Game_Engine.Scripts.Data
                 return backgroundColor == level.backgroundColor;
             }
             return false;
+        }
+
+        public string ToLevelScript()
+        {
+            string configTemplateDir = "Engine Resources\\Script Templates\\LevelScriptTemplate.cs";
+            if (File.Exists(configTemplateDir))
+            {
+                string script = File.ReadAllText(configTemplateDir);
+
+                string beginScript = "";
+                beginScript += "BackgroundColor = Color.FromArgb(" + backgroundColor.A + ", " + backgroundColor.R + ", " + backgroundColor.G + ", " + backgroundColor.B + ");\r\n";
+                // TODO : Insert Game Object instantiation
+                script = script.Replace("// __begin_code__", beginScript);
+
+                script = script.Replace("Color.White; // __background_texture_color__", "Color.FromArgb(" + backgroundTextureTint.A + ", " + backgroundTextureTint.R + ", " + backgroundTextureTint.G + ", " + backgroundTextureTint.B + ");");
+
+                script = script.Replace("__bg_texture_path_name__", backgroundTexture);
+
+                return script;
+            }
+            return null;
         }
     }
 }
