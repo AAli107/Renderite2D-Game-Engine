a778fd39a1632a2543e040d1ed2c50dc6c71a28a
Ali107 <Alialawadhi2014bh@gmail.com>
2024-11-30 16:47:20 +0300

    added OpenTK.Mathematics and OpenTK.Windowing.GraphicsLibraryFramework as using statements within template scripts.

--- Renderite2D Game Engine/Engine Resources/Script Templates/CustomScriptTemplate.cs
+++ Renderite2D Game Engine/Engine Resources/Script Templates/CustomScriptTemplate.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using Renderite2D_Project.Renderite2D;
 using Renderite2D_Project.Renderite2D.Components;
 using Renderite2D_Project.Renderite2D.Components.RenderComponents;
